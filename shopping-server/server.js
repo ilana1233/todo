cf0cf76a09df1203b2dc4d7b474582e3af4d32a2
ilana_trabelsi <ilana221eilat@gmail.com>
2025-07-14 21:08:11 +0300

    פרויקט מתכונים עם react and express

--- shopping-server/server.js
+++ shopping-server/server.js
@@ -40,7 +40,7 @@ app.delete('/api/items/:id', (req, res) => {
 });
 
 
-app.get('*', (req,res) => {
+app.get('/', (req,res) => {
   res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
 });
 

3548f597e2eba0b1eb582bbc55935353b57ea664
ilana_trabelsi <ilana221eilat@gmail.com>
2025-07-14 22:17:20 +0300

    react and express

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+  }, 
+
   plugins: [react()],
   server: {
-    port:5173
+    proxy: {
+  '/api':
+  'http://localhost:5000'
+    }
+    
   },
 })

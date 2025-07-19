aaee6d389c2ef7c3763ba2fa61eb3390edbc8ace
ilana_trabelsi <ilana221eilat@gmail.com>
2025-07-19 21:27:09 +0300

    Add build

--- shopping-server/server.js
+++ shopping-server/server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const recipeRoutes = require('./Routes/recipeRoutes');
 const path = require('path');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const PORT =  process.env.PORT || 5000
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("Connected to mongodb"))
+.catch((err) => console.error("Mongodb connection error", err));
 
 const app = express();
 app.use(cors());

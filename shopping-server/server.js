596724f4ac0e5c9c240d3197d478fb043c62317c
ilana_trabelsi <ilana221eilat@gmail.com>
2025-07-19 21:42:24 +0300

    Add build

--- shopping-server/server.js
+++ shopping-server/server.js
@@ -47,10 +47,10 @@ app.delete('/api/items/:id', (req, res) => {
 
 app.use('/api', recipeRoutes);
 
-app.use(express.static(path.join(__dirname,'../dist/shopping-client')));
+app.use(express.static(path.join(__dirname,'../dist')));
 
 app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, '../dist/shopping-client','index.html'));
+  res.sendFile(path.join(__dirname, '../dist','index.html'));
 });
 
 
